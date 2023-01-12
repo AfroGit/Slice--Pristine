6d4a4eb49097088d418387bd34084b549f82f139
Maurice Mutua <94635472+AfroGit@users.noreply.github.com>
2023-01-12 22:17:17 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// let arr = [t, e, s, t];
+
+// console.log( arr.slice(1, 3) ); // e,s (copy from 1 to 3)
+
+
+
+const strings = ["11", "12", "13"];
+const digitss = strings.map(Number);
+
+console.log(digitss);
